6f28ce9fc1cb48c5b981093f2519d7238966407a
Marinac <nikola@regex-sh.com>
2021-08-18 15:49:00 +0200

    Project init

---
+++ lib/imap_ex.ex
@@ -0,0 +1,18 @@
+defmodule ImapEx do
+  @moduledoc """
+  Documentation for `ImapEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ImapEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/imap_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule ImapExTest do
+  use ExUnit.Case
+  doctest ImapEx
+
+  test "greets the world" do
+    assert ImapEx.hello() == :world
+  end
+end
